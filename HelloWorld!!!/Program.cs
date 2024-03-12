c6fb9ee76f9ebe8572dac7207bbe655b6206ed93
ZKDS <nikitoc0@mail.ru>
2024-03-12 09:08:21 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld!!!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        Write("Hello");
+        Write(" world");
+        Write(" !!!");
+
+        ReadLine();
+    }
+}
